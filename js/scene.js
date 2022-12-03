3804dd3be9815088309de92230d0a0ed74b814e0
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2022-12-03 12:21:23 +0000

    create Particle class

--- js/scene.js
+++ js/scene.js
@@ -1,1 +1,59 @@
-console.log("Test scene 1234");
+/*
+The animation in inspired by this video tutorial: 
+https://www.youtube.com/watch?v=d620nV6bp0A
+*/
+"use strict";
+
+// get DOOM elements and create variables
+const canvas = document.getElementById("canvas-scene");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray;
+
+// mouse interaction
+
+let mouse = {
+  x: null,
+  y: null,
+  radius: (canvas.height / 80) * (canvas.width / 80),
+};
+
+window.addEventListener("mousemove", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+// particles
+class Particle {
+  constructor(x, y, directionX, directionY, size, color) {
+    this.x = x;
+    this.y = y;
+    this.directionX = directionX;
+    this.directionY = directionY;
+    this.size = size;
+    this.color = color;
+  }
+
+  // method to draw individual particle
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+    ctx.fillStyle = "#bc5523";
+    ctx.fill();
+  }
+
+  // check particle position, check mouse position, move the particles, draw the particle
+  update() {
+    //check if particles is still within canvas
+    if (this.x > canvas.width || this.x < 0) {
+      this.directionX = -this.directionX;
+    }
+    if (this.y > canvas.height || this.y < 0) {
+      this.directionY = -this.directionY;
+    }
+  }
+}
+
+console.log(Particle, mouse, canvas, ctx);
